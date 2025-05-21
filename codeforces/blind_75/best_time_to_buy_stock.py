86117fab3900cafa93ea8c3841b02f23c20c102c
Adrian <adijos126@gmail.com>
2025-05-21 21:43:08 +0200

    Add some scripts

--- codeforces/blind_75/best_time_to_buy_stock.py
+++ codeforces/blind_75/best_time_to_buy_stock.py
@@ -1,16 +1,32 @@
 class Solution:
     
     def maxProfit(self, prices):
+        l = 0 
+        r = 1
+        maxP = 0
+        while r < len(prices):
+            if prices[l] < prices[r]:
+                profit = prices[r] - prices[l]
+                maxP = max(maxP, profit)
+            else:
+                l = r
+            r += 1
+        return maxP
 
-    l, r = 0, 1
-    maxP = 0
+sol = Solution() 
+print(sol.maxProfit([1,2,3,4,5]))
 
-    while r < len(prices):
-        if prices[l] < prices[r]:
-            print = prices[r] = prices[l]
-            maxP = max(maxP, profit)
-        else:
-            l = r
-        r += 1
-    
-    return maxP
+
+#second solution
+
+
+prices = [1,2,3,4,5]
+l = 0
+maxPrice1 = 0
+for r in range(1, len(prices)):
+    if prices[r] > prices[l]:
+        profit = prices[r] - prices[l]
+        maxPrice1 = max(maxPrice1, profit)
+    else:
+        l = r
+print(maxPrice1)
